98242f9bc9b4725705b8bdd1876f9ecd41fbe277
Đào Vũ Đạt <dat.daovu@gmail.com>
2022-01-17 11:45:21 +0700

    add href into icon and hover effect

--- components/Overview/Overview.tsx
+++ components/Overview/Overview.tsx
@@ -4,26 +4,46 @@ import TypeScriptIcon from '@/components/Overview/TypeScriptIcon';
 import TailwindIcon from '@/components/Overview/TailwindIcon';
 import NetCoreIcon from '@/components/Overview/NetCoreIcon';
 
+const listIconStyle = `border-2 p-4 rounded-lg border-transparent hover:border-secondary_hover transition duration-300 cursor-pointer`;
+
 const Overview = () => {
   return (
     <div className="mt-8 pt-8">
       <div className="container m-auto">
-        <p className="ml-24 text-lg text-secondary_text">Experience:</p>
-        <ul className="mt-8 flex space-x-8 h-32 justify-between">
-          <li>
-            <ReactIcon />
+        <p className="ml-24 tracking-wide text-secondary_text">Experience :</p>
+        <ul className="mt-16 flex space-x-8 h-32 justify-between">
+          <li className={listIconStyle}>
+            <a href="https://www.reactjs.org" target="_blank" rel="noreferrer">
+              <ReactIcon />
+            </a>
           </li>
-          <li>
-            <NextJsIcon />
+          <li className={listIconStyle}>
+            <a href="https://www.nextjs.org" target="_blank" rel="noreferrer">
+              <NextJsIcon />
+            </a>
           </li>
-          <li>
-            <TypeScriptIcon />
+          <li className={listIconStyle}>
+            <a
+              href="https://www.typescriptlang.org/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <TypeScriptIcon />
+            </a>
           </li>
-          <li>
-            <TailwindIcon />
+          <li className={listIconStyle}>
+            <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer">
+              <TailwindIcon />
+            </a>
           </li>
-          <li>
-            <NetCoreIcon />
+          <li className={listIconStyle}>
+            <a
+              href="https://dotnet.microsoft.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <NetCoreIcon />
+            </a>
           </li>
         </ul>
       </div>
